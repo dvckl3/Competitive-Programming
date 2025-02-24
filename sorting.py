2a32d79549127f0cd59dab23a0faddd84dcb5748
Le Tri Duc <letriduc.dn@gmail.com>
2025-02-24 23:03:24 +0700

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -10,4 +10,20 @@ def interchange_sort(a):
 
 
 def quick_sort(a):
+    if len(a)<=1:
+        return a
+    pivot=a[len(a)//2]
+    left = [x for x in a if x<pivot]
+    middle = [x for x in a if x==pivot]
+    right = [x for x in a if x>pivot]
+    return quick_sort(left)+middle+quick_sort(right)
+    
+def bubble_sort(a):
+    n=len(a)
+    for i in range(n):
+        for j in range(n-i-1):
+            if a[j]>a[j+1]:
+                a[j],a[j+1]=a[j+1],a[j]
+    return a
+    
     
